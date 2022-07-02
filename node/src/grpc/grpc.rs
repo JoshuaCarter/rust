db6f27dd29ac4b7945c9ad2a8a167231a87042b2
jcarter <jcarter.dev@pm.me>
2022-07-02 15:43:01 +1000

    Add trivial auth

--- node/src/grpc/grpc.rs
+++ node/src/grpc/grpc.rs
@@ -1,14 +1,33 @@
 use std::{net::SocketAddr, str::FromStr};
 use anyhow::Result;
 use infra::model::trading::trading_server::*;
-use tonic::transport::Server;
+use tonic::{transport::Server, metadata::{MetadataValue, Ascii}, Status, service::Interceptor};
 use super::trading::TradingService;
+
+#[derive(Clone)]
+pub struct GrpcInterceptor {
+    token: MetadataValue<Ascii>,
+}
+
+impl Interceptor for GrpcInterceptor {
+    fn call(&mut self, req: tonic::Request<()>) -> Result<tonic::Request<()>, Status> {
+        if let Some(t) = req.metadata().get("authorization") {
+            if self.token == t {
+                return Ok(req);
+            }
+        }
+        return Err(Status::unauthenticated("No valid auth token"));
+    }
+}
 
 pub async fn start_server(uri: &str) -> Result<()> {
     let addy = SocketAddr::from_str(uri)?;
+    let interceptor = GrpcInterceptor {
+        token: "TOKEN".parse()?,
+    };
 
     Server::builder()
-        .add_service(TradingServer::new(TradingService::default()))
+        .add_service(TradingServer::with_interceptor(TradingService::default(), interceptor))
         .serve(addy)
         .await?;
 
--- node/src/venues/factory.rs
+++ node/src/venues/factory.rs
@@ -3,6 +3,36 @@ use infra::model::venue::Venue;
 use infra::model::common::Exchange;
 use super::Binance;
 
+// Q: Will we need to persist and share venue instances between calls?
+
+// static BINANCE: Mutex<Binance> = Mutex::new(Binance::new());
+
+// pub async fn make_venue(exchange: Exchange) -> Option<&'static Mutex<Binance>> {
+//     match exchange {
+//         Exchange::Binance => {
+//             return Some(&BINANCE);
+//         },
+//         _ => { return None; }
+//     }
+// }
+
+// type VenueContainer = Mutex<Box<dyn Venue>>;
+
+// static mut VENUES: HashMap<Exchange, VenueContainer> = HashMap::new();
+
+// pub async fn make_venue(exchange: Exchange) -> Option<&'static VenueContainer> {
+//     if !VENUES.contains_key(&exchange) {
+//         match exchange {
+//             Exchange::Binance => {
+//                 VENUES.insert(exchange, Mutex::new(Box::new(Binance::new())));
+//             },
+//             _ => { return None; }
+//         }
+//     }
+
+//     return VENUES.get(&exchange);
+// }
+
 pub fn create_venue(exchange: Exchange) -> Result<impl Venue> {
     match exchange {
         Exchange::Binance => { return Ok(Binance::new()); },
